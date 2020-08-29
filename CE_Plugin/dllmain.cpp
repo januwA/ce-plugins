df41de1d190e5b257470879d0a69ceebbdf991e1
januwA <ajanuw1995@gmail.com>
2020-08-30 01:21:29 +0800

    修复获取窗口出错

--- CE_Plugin/dllmain.cpp
+++ CE_Plugin/dllmain.cpp
@@ -3,40 +3,49 @@
 #include <Windows.h>
 #include <iostream>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
 extern "C" __declspec(dllexport) HWND targetWindow = 0;
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 {
-    DWORD  dwProcessID = 0;
-    GetWindowThreadProcessId(hwnd, &dwProcessID);
-	if (dwProcessID == GetCurrentProcessId())
-    {
-        targetWindow = hwnd;
-        return false;
-    }
-    return true;
+	DWORD  dwProcessID = 0;
+	GetWindowThreadProcessId(hwnd, &dwProcessID);
+	if (dwProcessID == GetCurrentProcessId()) // 判断pid
+	{
+		if (!GetParent(hwnd)) // 是顶级窗口
+		{
+			char text[1024];
+			GetWindowTextA(hwnd, (LPSTR)text, 1024); // 必须含有标题文字
+			if (strlen(text) != 0)
+			{
+				// printf("%s\n", text);
+				targetWindow = hwnd;
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 extern "C" {
 	__declspec(dllexport) HWND __stdcall getTargetWindow()
-    {
-        if (targetWindow) return targetWindow;
-        EnumWindows(EnumWindowsProc, 0);
-        return targetWindow;
-    }
+	{
+		if (targetWindow) return targetWindow;
+		EnumWindows(EnumWindowsProc, 0);
+		return targetWindow;
+	}
 }
